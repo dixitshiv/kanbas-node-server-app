9748775f15ed251043ff9e539287a6d5c37263b3
dixitshiv <145235016+dixitshiv@users.noreply.github.com>
2023-12-16 17:50:03 +0530

    A6

---
+++ app.js
@@ -0,0 +1,66 @@
+// const express = require("express");
+import express from "express";
+import HelloRoutes from "./hello.js";
+import Lab5 from "./lab5.js";
+import cors from "cors";
+import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./modules/routes.js";
+import AssignmentRoutes from "./assignments/routes.js";
+import dotenv from "dotenv";
+import "dotenv/config";
+import session from "express-session";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+
+const CONNECTION_STRING =
+  "mongodb+srv://dshivam625:b4cooTULFNFnkSaK@newkanbas.lool9s7.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.connect(CONNECTION_STRING);
+
+const app = express();
+
+// const allowedOrigins = ["https://a5--frabjous-mochi-2114eb.netlify.app/", process.env.FRONTEND_URL];
+// const corsOptions = {
+//   credentials: true,
+//   origin: (origin, callback) => {
+//     if (allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+// };
+// app.use(cors(corsOptions));
+
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  })
+);
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+app.use(session(sessionOptions));
+
+app.use(express.json());
+
+UserRoutes(app);
+ModuleRoutes(app);
+CourseRoutes(app);
+AssignmentRoutes(app);
+Lab5(app);
+HelloRoutes(app);
+
+dotenv.config();
+
+// listen on the port or default to 4000.
+app.listen(process.env.PORT || 4000);
